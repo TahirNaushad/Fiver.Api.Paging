293a9dfe4af5b175b75284928aae3c364e137fc8
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-05 10:42:35 +0100

    added default page number and size

--- Fiver.Api.Paging/Lib/PagingParams.cs
+++ Fiver.Api.Paging/Lib/PagingParams.cs
@@ -2,7 +2,7 @@
 {
     public class PagingParams
     {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
     }
 }
